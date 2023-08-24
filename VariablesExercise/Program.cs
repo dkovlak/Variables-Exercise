7460b4e5bd6fd2e94fa596c11113ef49e6a957ba
David Kovlak <kovlakdavid4@gmail.com>
2023-08-24 14:23:08 -0700

    NewYork

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string cityName; //Declaration syntax
+
+            cityName = "New York"; //Initialized --- assigning a value
+
+            int cityAge = 399; //Declaring and initializing
+
+            char firstInitial = 'N';
+
+            bool isUSA = true;
+
+            double landArea = 302.6;
+
+            decimal population = 8804190;
+
+            Console.WriteLine($"Hey, do you want to visit {cityName} City in the USA? Its first initial {firstInitial}");
+            Console.WriteLine($"This land was found {cityAge} years ago.");
+            Console.WriteLine($"{cityName} spans {landArea} square miles, and moreover, the population is {population}. And yes it is {isUSA}");
         }
     }
 }
